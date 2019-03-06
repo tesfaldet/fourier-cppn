7a5bf2854cdd64dde2e1661caa9a9be521b711aa
tboquet <thomas.boquet@hec.ca>
2019-03-06 16:26:32 -0500

    * fix train.py

--- train.py
+++ train.py
@@ -17,8 +17,8 @@ parser.add_argument('-lr', '--learning_rate', default=5e-3, type=float)
 parser.add_argument('-logf', '--log_frequency', default=10, type=int)
 parser.add_argument('-printf', '--print_frequency', default=10, type=int)
 parser.add_argument('-snapf', '--snapshot_frequency', default=5000, type=int)
-parser.add_argument('-log_dir', '--log_dir', default=".logs", type=int)
-parser.add_argument('-snap_dir', '--snapshot_dir', default=".snapshots", type=int)
+parser.add_argument('-log_dir', '--log_dir', default=".logs", type=str)
+parser.add_argument('-snap_dir', '--snapshot_dir', default=".snapshots", type=str)
 parser.add_argument('-id', '--run_id', default=time.strftime('%d%b-%X'),
                     type=str)
 args = parser.parse_args()
@@ -33,7 +33,7 @@ my_config['log_frequency'] = args.log_frequency
 my_config['run_id'] = args.run_id
 my_config['iterations'] = args.iterations
 my_config['log_dir'] = args.log_dir
-my_config['snap_dir'] = args.snap_dir
+my_config['snap_dir'] = args.snapshot_dir
 
 # SHURIKEN MAGIC
 my_config.update(get_hparams())
